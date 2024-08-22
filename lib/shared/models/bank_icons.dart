388fa43ea1dda712b9cd8e9d232274d83c0f70b2
thgoas <thgas95@gmail.com>
2024-08-22 20:24:41 -0300

    feat: create model BankIcons

---
+++ lib/shared/models/bank_icons.dart
@@ -0,0 +1,79 @@
+class BankIcons {
+  final String name;
+  final String path;
+  BankIcons({required this.name, required this.path});
+}
+
+final listBanks = [
+  BankIcons(name: 'ABC Brasil', path: 'images/banks/abc.svg'),
+  BankIcons(name: 'Ailos', path: 'images/banks/ailos.svg'),
+  BankIcons(name: 'Asaas IP S.A', path: 'images/banks/asaas_ip_s.a.svg'),
+  BankIcons(name: 'Banco BS2 S.A', path: 'images/banks/bs2.svg'),
+  BankIcons(name: 'Banco BTG Pacutal', path: 'images/banks/btg_pacutal.svg'),
+  BankIcons(name: 'Banco C6 S.A', path: 'images/banks/c6_bank.svg'),
+  BankIcons(
+      name: 'Banco da Amazônia S.A',
+      path: 'images/banks/banco_da_amazonia.svg'),
+  BankIcons(name: 'Banco Daycoval', path: 'images/banks/daycoval.svg'),
+  BankIcons(
+      name: 'Banco do Brasil S.A', path: 'images/banks/banco_do_brasil.svg'),
+  BankIcons(
+      name: 'Banco do Estado do Espirito Santo',
+      path: 'images/banks/banestes.svg'),
+  BankIcons(name: 'Banco do Estado do Para', path: 'images/banks/banpara.svg'),
+  BankIcons(
+      name: 'Banco do Estado do Sergipe', path: 'images/banks/banese.svg'),
+  BankIcons(
+      name: 'Banco do Nordeste do Brasil S.A', path: 'images/banks/bnb.svg'),
+  BankIcons(
+      name: 'Banco Industrial do Brasil S.A', path: 'images/banks/bib.svg'),
+  BankIcons(name: 'Banco Inter S.A', path: 'images/banks/inter.svg'),
+  BankIcons(
+      name: 'Banco Mercantil do Brasil',
+      path: 'images/banks/banco_mercantil.svg'),
+  BankIcons(
+      name: 'Banco Original S.A', path: 'images/banks/banco_original.svg'),
+  BankIcons(name: 'Banco Pine', path: 'images/banks/banco_pine.svg'),
+  BankIcons(
+      name: 'Banco Rendimento', path: 'images/banks/banco_rendimento.svg'),
+  BankIcons(name: 'Banco Safra S.A', path: 'images/banks/safra.svg'),
+  BankIcons(
+      name: 'Banco Santander Brasil S.A',
+      path: 'images/banks/banco_santander.svg'),
+  BankIcons(name: 'Banco Sofisa', path: 'images/banks/sofisa.svg'),
+  BankIcons(name: 'Banco Topazio', path: 'images/banks/banco_topazio.svg'),
+  BankIcons(
+      name: 'Banco Triângulo - Tribanco', path: 'images/banks/tri_banco.svg'),
+  BankIcons(name: 'Bank of America', path: 'images/banks/bank_of_america.svg'),
+  BankIcons(name: 'Banrisul', path: 'images/banks/banrisul.svg'),
+  BankIcons(name: 'Bradesco S.A', path: 'images/banks/bradesco.svg'),
+  BankIcons(name: 'BRB - Banco de Brasília', path: 'images/banks/brb.svg'),
+  BankIcons(
+      name: 'Caixa Econômica Federal',
+      path: 'images/banks/caixa_economica_federal.svg'),
+  BankIcons(name: 'Capitual', path: 'images/banks/capitual.svg'),
+  BankIcons(
+      name: 'Conta Simples Soluções em Pagamentos',
+      path: 'images/banks/conta_simples.svg'),
+  BankIcons(
+      name: 'Cora Sociedade Credito Direto S.A', path: 'images/banks/cora.svg'),
+  BankIcons(name: 'Credisis', path: 'images/banks/credisis.svg'),
+  BankIcons(name: 'Cresol', path: 'images/banks/cresol.svg'),
+  BankIcons(name: 'Efí - Gerencianet', path: 'images/banks/efi_bank.svg'),
+  BankIcons(name: 'Grafeno', path: 'images/banks/grafeno.svg'),
+  BankIcons(name: 'Itaú Unibanco S.A', path: 'images/banks/itau.svg'),
+  BankIcons(name: 'Lets Bank S.A', path: 'images/banks/lets_bank.svg'),
+  BankIcons(name: 'Mercado Pago', path: 'images/banks/mercado_pago.svg'),
+  BankIcons(name: 'Nubank', path: 'images/banks/nubank.svg'),
+  BankIcons(name: 'Omie Cash', path: 'images/banks/omie.svg'),
+  BankIcons(
+      name: 'PagSeguro Internet S.A', path: 'images/banks/pag_seguro.svg'),
+  BankIcons(
+      name: 'Quality Digital Bank',
+      path: 'images/banks/quality_digital_bank.svg'),
+  BankIcons(name: 'Sicoob', path: 'images/banks/sicoob.svg'),
+  BankIcons(name: 'Sicredi', path: 'images/banks/sicredi.svg'),
+  BankIcons(name: 'Stone Pagamentos S.A', path: 'images/banks/stone.svg'),
+  BankIcons(name: 'Unicred', path: 'images/banks/unicred.svg'),
+  BankIcons(name: 'Uniprime', path: 'images/banks/uniprime.svg'),
+];
